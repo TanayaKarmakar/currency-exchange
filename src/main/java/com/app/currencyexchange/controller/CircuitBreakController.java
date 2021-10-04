316a2dd20942327ab7b0792041145858a977e54e
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-10-04 08:51:10 +0530

    added resilience 4j

--- src/main/java/com/app/currencyexchange/controller/CircuitBreakController.java
+++ src/main/java/com/app/currencyexchange/controller/CircuitBreakController.java
@@ -1,5 +1,7 @@
 package com.app.currencyexchange.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +19,13 @@ public class CircuitBreakController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    //@CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
     public String sampleApi() {
         logger.info("Sample API call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:9091/some-dummy-url", String.class);
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:9091/some-dummy-url", String.class);
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     private String hardcodedResponse(Exception ex) {
